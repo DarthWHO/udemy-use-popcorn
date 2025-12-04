068b614fe485ead082b92999f0c77e50d12cf164
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-03 19:25:44 -0700

    implemented useRef to focus on search element

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputElement.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return function () {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -8,6 +27,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
